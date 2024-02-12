21f595b9c47ce0dfcc2a727d67ed6f18e58372bc
hiverkiya <hiverkiya@gmail.com>
2024-02-12 16:19:12 +0530

    Implementing parallel routing

--- lib/fetchSearch.ts
+++ lib/fetchSearch.ts
@@ -3,7 +3,9 @@ import { Result } from "@/typings/searchTypings";
 function fetchSearch(searchTerm: string) {
   const username = process.env.OXYLABS_USERNAME;
   const password = process.env.OXYLABS_PASSWORD;
+
   const newUrl = new URL(`https://www.walmart.com/search?q=${searchTerm}`);
+
   const body = {
     source: "universal_ecommerce",
     url: newUrl.toString(),
@@ -19,14 +21,20 @@ function fetchSearch(searchTerm: string) {
       Authorization:
         "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
     },
+    next: {
+      revalidate: 60 * 60 * 24,
+    },
   })
     .then((res) => res.json())
     .then((data) => {
       if (data.results.length === 0) return;
       const result: Result = data.results[0];
+
       return result;
     })
     .catch((err) => console.log(err));
+
   return response;
 }
+
 export default fetchSearch;
